f790657c22838d2dc992275cfb6c029502b9bc6a
Steven Foster Jr <Steven Foster Jr>
2017-08-04 22:43:46 -0700

    Initial Load

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', False);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', False);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', False);
